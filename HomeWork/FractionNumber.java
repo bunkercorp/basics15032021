338f39a9401d10d75855d98f61f189c613808c5b
Pismichenko <misha.prog@gmail.com>
2021-04-17 20:33:20 +0300

    first homw work test

---
+++ HomeWork/FractionNumber.java
@@ -0,0 +1,71 @@
+public class FractionNumber {
+
+    private int numerator;
+    private int denominator;
+
+    public FractionNumber(int numerator, int denominator){
+        int reduction = numerator;
+        numerator = (numerator < 0 ) ? numerator *= -1 : numerator;
+        denominator = (denominator < 0 ) ? denominator *= -1 : denominator;
+
+        for(; reduction > 1; reduction--){
+            if( numerator / reduction != (double)numerator / reduction && denominator / reduction != (double)denominator / reduction))
+                break;
+        }
+        this.numerator = numerator / reduction;
+        this.denominator = denominator / reduction;
+    }
+
+    @Override
+    public String toString() {
+        if(denominator == 1)
+            return String.format("%d", numerator);
+        else if(denominator == 0)
+            return "DIVISION BY ZERO";
+        else if(numerator < denominator)
+            return String.format("%d/%d", numerator, denominator);
+        else if(numerator >= denominator)
+            return String.format("%d %d/%d", numerator/denominator, numerator, denominator);
+
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass() != FractionNumber.class || obj == null)
+            return false;
+        FractionNumber newObj =(FractionNumber) obj;
+        return this.numerator == newObj.numerator && this.denominator == newObj.denominator;
+    }
+
+
+    public FractionNumber add(FractionNumber fn){
+        if(fn == null) return null;
+
+        if(this.denominator == fn.denominator)
+            return new FractionNumber(this.numerator + fn.numerator, this.denominator);
+
+        return new FractionNumber(this.numerator * fn.denominator + fn.numerator * this.denominator, this.denominator * fn.denominator);
+    }
+
+    public FractionNumber subtract(FractionNumber fn){
+        if(fn == null) return null;
+
+        if(this.denominator == fn.denominator)
+            return new FractionNumber(this.numerator - fn.numerator, this.denominator);
+
+        return new FractionNumber(this.numerator * fn.denominator - fn.numerator * this.denominator, this.denominator * fn.denominator);
+    }
+
+    public FractionNumber multiply(FractionNumber fn){
+        if(fn == null) return null;
+
+        return new FractionNumber(this.numerator *  fn.numerator, this.denominator * fn.denominator);
+    }
+
+    public FractionNumber divide(FractionNumber fn){
+        if(fn == null) return null;
+
+        return new FractionNumber(this.numerator *  fn.denominator, this.denominator * fn.numerator);
+    }
+}

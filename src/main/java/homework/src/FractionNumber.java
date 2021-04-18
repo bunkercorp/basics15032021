678ce09f7f4c3443b49b6115da2b663b80a7e31c
Samigullin Nikita <nikita.samigullin@travisperkins.co.uk>
2021-04-18 20:26:40 +0300

    Added comments

--- src/main/java/homework/src/FractionNumber.java
+++ src/main/java/homework/src/FractionNumber.java
@@ -3,9 +3,10 @@
     private int numerator;
 
     private int denominator;
-
+    // ДЗ не дает пользователю выбора, сокращать дробь или нет.
     public Fraction(int numerator,int denominator,boolean wantToReduce) {
         if (denominator == 0) {
+    // ДЗ дает пространство решить эту проблему без применения механизмов, с которыми мы еще не ознакомились
             throw new IllegalArgumentException("The denominator is zero.");
         }
         if(numerator==0){
@@ -20,6 +21,7 @@
             this.numerator = -1*this.numerator;
             this.denominator = -1*this.denominator;
         }
+        // if(wantToReduce) это законно
         if(wantToReduce==true)
             this.reduce();
     }
@@ -28,6 +30,7 @@
         this.denominator = 1;
     }
 
+    // ДЗ навязывает 4 ситуации. Здесь учтены две
     @Override
     public String toString() {
         if(denominator!=1)
@@ -43,6 +46,7 @@
         Fraction f = ((Fraction)obj);
         int gcd =gcd(numerator,denominator);
         f.reduce();
+         // return (this.numerator/gcd==f.numerator) && (this.denominator/gcd == f.denominator); это законно
         if(this.numerator/gcd==f.numerator && this.denominator/gcd == f.denominator)
             return true;
         else
@@ -70,6 +74,14 @@
     }
 
     //Cannot decide weather to make static methods or not so I randomly picked one.
+
+     // мы обсуждали этот вопрос  при постановке ДЗ. Эти методы должны быть у инстанций, то есть, они НЕ статичны.
+     // как эти методы должны применяться:
+     /*
+     FractionNumber a = new FractionNumber(1,2);
+     FractionNumber b = new FractionNumber(3,5);
+     FractionNumber c = a.add(b);
+     */
     public static Fraction add(Fraction f1,Fraction f2,boolean w){
         return new Fraction(f1.numerator*f2.denominator+f2.numerator*f1.denominator,f1.denominator*f2.denominator,w);
     }

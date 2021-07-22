864c108ad7d2ac3c7546a970dde355ef16420702
Alex Lyulka <sunmanodessa@ukr.net>
2021-07-22 21:25:24 +0300

    21

--- src/test/java/Grid_2.java
+++ src/test/java/Grid_2.java
@@ -1,6 +1,8 @@
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -15,22 +17,47 @@ import java.net.URL;
 public class Grid_2 {
 
     WebDriver driver;
-    String baseUrl, nodeUrl;
+    final String baseUrl = "https://www.saucedemo.com/", nodeUrl = "http://10.0.0.69:4444/wd/hub";
+
     @BeforeTest
+
     public void setup() throws MalformedURLException {
-    baseUrl = "https://www.saucedemo.com/" ;
-    nodeUrl = "http://10.0.0.69:4444/wd/hub";
-        DesiredCapabilities capability = DesiredCapabilities.firefox();
-        capability.setBrowserName("firefox");
-     //   capability.setPlatform(Platform.WIN10);
-        driver = new RemoteWebDriver(new URL(nodeUrl), capability);
+        // DesiredCapabilities capability = DesiredCapabilities.firefox();
+        //capability.setBrowserName("firefox");
+        //   capability.setPlatform(Platform.WIN10);
+        // driver = new RemoteWebDriver(new URL(nodeUrl), capability);
+        final boolean isCiRun = System.getenv("CI_RUN") != null;
+        final String browserRequested = System.getProperty("browser");
+        if (isCiRun) {
+            DesiredCapabilities capability;
+            if (browserRequested.contentEquals("firefox")) {
+                capability = DesiredCapabilities.firefox();
+            } else {
+                capability = DesiredCapabilities.chrome();
+            }
+            driver = new RemoteWebDriver(new URL(nodeUrl), capability);
+        } else {
+            if (browserRequested.contentEquals("firefox")) {
+                System.setProperty("webdriver.gecko.driver","C:\\Applications\\geckodriver\\geckodriver.exe");
+                driver = new FirefoxDriver();
+
+            } else {
+                System.setProperty("webdriver.chrome.driver","C:\\Applications\\chromedriver\\chromedriver.exe");
+                driver  = new ChromeDriver();
+
+            }
+
+
+        }
     }
-//    @AfterTest
+
+
+    //    @AfterTest
 //    public void afterTest(){
 //        driver.quit();
 //    }
     @Test
-    public void simpleTest(){
+    public void simpleTest() {
         driver.get(baseUrl);
 
     }
